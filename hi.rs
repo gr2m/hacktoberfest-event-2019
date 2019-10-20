938216b07803466a126dc584189fdc7d729e1e30
recalcitrant-raccoon <49954078+recalcitrant-raccoon@users.noreply.github.com>
2019-10-20 13:52:22 -0700

    Update hi.rs to correct bad info
    Explained my mistake.

--- hi.rs
+++ hi.rs
@@ -1,20 +1,26 @@
 use std::io
 
+//In order to run this on linux you'd want to apt install rustc, or yum or rpm or whatever you use
+//To see more instructions you should see the Rust documentation at this link:
+//https://www.rust-lang.org/tools/install
+//Rust is built to be interoperable with git, and builds .gitignore and 
 fn main(){
-  println!("H");
-  println!("e");
-  println!("l");
-  println!("l");
-  println!("o");
-  println!(" ");
-  println!("H");
-  println!("a");
-  println!("c");
-  println!("k");
-  println!("t");
-  println!("o");
-  println!("b");
-  println!("e");
-  println!("r");
-  println!("fest!");
+  println!("I was wrong, when you use the println!() function it adds a linebreak on its own.");
+  println!("So it's not like cout, which is probably a good thing.");
+  print!("H");
+  print!("e");
+  print!("l");
+  print!("l");
+  print!("o");
+  print!(" ");
+  print!("H");
+  print!("a");
+  print!("c");
+  print!("k");
+  print!("t");
+  print!("o");
+  print!("b");
+  print!("e");
+  print!("r");
+  print!("fest!");
 }
