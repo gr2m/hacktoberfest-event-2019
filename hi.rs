50d3e279f3ea5cc7fef0758b13f056278455220c
recalcitrant-raccoon <49954078+recalcitrant-raccoon@users.noreply.github.com>
2019-10-20 13:25:30 -0700

    Creating a new rust file in the repo
    Just a hello world in (almost) the least efficient way possible.

---
+++ hi.rs
@@ -0,0 +1,20 @@
+use std::io
+
+fn main(){
+  println!("H");
+  println!("e");
+  println!("l");
+  println!("l");
+  println!("o");
+  println!(" ");
+  println!("H");
+  println!("a");
+  println!("c");
+  println!("k");
+  println!("t");
+  println!("o");
+  println!("b");
+  println!("e");
+  println!("r");
+  println!("fest!");
+}
